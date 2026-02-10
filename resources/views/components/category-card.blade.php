5abf3c0e882ebf865f7bc40a68bce4d991e5769b
juan <parrajp1996@gmail.com>
2026-02-10 20:18:37 +0100

    corregido bug que no verificaba las imagenes en categorias

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -1,6 +1,15 @@
 <div class="bg-white rounded-lg shadow-lg overflow-hidden product-card cursor-pointer {{ $class }}">
     <div class="h-40 bg-gray-200 flex items-center justify-center">
-        <img src="{{ $category->image }}" alt="{{ $category->name }}" class="w-full h-full object-cover">
+        @if($category->image)
+            @php
+                $imageUrl = (str_starts_with($category->image, 'http://') || str_starts_with($category->image, 'https://'))
+                    ? $category->image
+                    : asset('storage/' . $category->image);
+            @endphp
+            <img src="{{ $imageUrl }}" alt="{{ $category->name }}" class="w-full h-full object-cover">
+        @else
+            <div class="h-16 w-16 rounded-md bg-gray-200 flex items-center justify-center text-gray-400 text-2xl">📁</div>
+        @endif
     </div>
     <div class="p-4">
         <h4 class="text-lg font-bold mb-2 text-gray-900">{{ $category->name }}</h4>
